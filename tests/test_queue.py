e2ebc379db66b0adc336df1698f0c0ba13fa3ba3
Mary <kass.o@yandex.ru>
2023-12-01 19:07:14 +0300

    tests from Queue class

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием unittest для модуля queue."""
+from src.queue import Queue
+
+
+def test_queue():
+    queue = Queue()
+    assert str(Queue()) == ""
+
+    queue.enqueue('data1')
+    queue.enqueue('data2')
+    queue.enqueue('data3')
+
+    assert queue.head.data == 'data1'
+    assert queue.head.next_node.data == 'data2'
+    assert queue.tail.data == 'data3'
+    assert queue.tail.next_node is None
+
+    assert str(queue) == "data1\ndata2\ndata3"
